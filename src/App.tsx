8ffdbcb83b5643a41ea1bbea1f7acae11be3759c
JAC-SERVERS\reza.shalchian <reza.shalchian@a.com>
2024-01-02 00:19:45 -0500

    adding grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  // return <Button colorScheme="blue">Button</Button>;
+  return (
+    // <Grid templateAreas={`"nav nav" "aside main"`}>
+    //   <GridItem area="nav" bg="coral">
+    //     Nav
+    //   </GridItem>
+    //   <GridItem area="aside" bg="gold">
+    //     Aside
+    //   </GridItem>
+    //   <GridItem area="main" bg="dodgerblue">
+    //     Main
+    //   </GridItem>
+    // </Grid>
+
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
