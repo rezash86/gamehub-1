cb43c9b2dcb5466e69248bf780d4b999cffff6d0
JAC-SERVERS\reza.shalchian <reza.shalchian@a.com>
2024-01-02 01:25:07 -0500

    adding dark mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -26,14 +26,11 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
+        {/* bg="gold" */}
+        <GridItem area="aside">Aside</GridItem>
       </Show>
-
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
+      {/* bg="dodgerblue" */}
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
